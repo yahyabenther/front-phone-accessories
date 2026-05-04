c6f14a17d3d81b2645dc4a7992114d108fdd7238
Oswa-hub <harouni.oswa@gmail.com>
2026-05-04 22:21:52 +0100

    feat: categories CRUD frontend + UI

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CategoriesComponent } from './pages/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: CategoriesComponent },      // admin = page d’accueil
+  { path: 'admin', component: CategoriesComponent }, // /admin = aussi admin
+  { path: '**', redirectTo: '' }
+];
